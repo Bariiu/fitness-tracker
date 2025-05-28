a4208d15809977da452c83ab875b211d94a47690
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-05-28 19:19:27 +0300

    seeded database

--- lib/models/user_workout.py
+++ lib/models/user_workout.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .__init__ import Base, Session, engine
